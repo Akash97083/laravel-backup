d09103bcfc9e258f227353d92026f7adad98009a
mamun <abdullah.ssc13@gmail.com>
2021-01-13 10:35:49 +0600

    factory file removed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,14 +12,5 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
-        factory(\App\User::class, 10)->create();
-
-        factory(App\Category::class, 50)->create()->each(function ($category) {
-            $posts = factory(App\Post::class, random_int(2, 8))->make();
-
-            print_r($posts->toArray());
-
-            $category->posts()->saveMany($posts);
-        });
     }
 }
